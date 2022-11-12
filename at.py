da839835fa72a04e382a2d170384cf6390e0c162
EllaLai <117461311+EllaLai@users.noreply.github.com>
2022-11-12 23:26:26 +0800

    Update at.py
    221112-01:修改油價取得

--- at.py
+++ at.py
@@ -43,3 +43,49 @@ flyp_c  =result_j["data"]["flights"][0]["priceInfo"][0]["from"]["currencyCode"]
 #flyin_t=str(flyin_t[:10])
 final_result = '星宇航空 STARLUX,'+flyout_a+', → ,'+flyin_a+', 經濟艙1張,'+flyout_t+', 起飛,'+flyin_t+', 抵達'+',票價 ,'+flyp_c+','+str(flyp_p)
 print(final_result)
+
+
+#以下是抓油價的
+now = datetime.now()           
+delta = timedelta(days=int(30))
+start_date = now + delta
+start_date = start_date.strftime('%Y-%m-%d')
+
+url="https://www2.moeaboe.gov.tw/oil111"
+headers = {
+'content-type' : 'text/html',
+'user-agent': 'Mozilla/5.0 (Windows NT 6.2; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/102.0.0.0 Safari/537.36',
+'jx-lang': 'zh-TW'
+}
+response = requests.post(url,headers=headers)
+response.encoding ='UTF-8'
+
+soup = BeautifulSoup(response.text, "html.parser")
+#print(soup.prettify())  #輸出排版後的HTML內容
+
+div_iwr = soup.find(class_="row row-cols-1 px-4")
+#print(div_iwr)
+#取西德州油價
+div_fn = div_iwr.find(class_="col-5 text-right")
+#print(div_fn)    
+div_fn2 = div_iwr.find_all("strong" ,limit=1)      #陣列
+wti_oil_price=""
+for wti_p in div_fn2:
+    wti_oil_price = wti_p.text
+    #print(wti_oil_price)
+    
+    
+#取更新日期
+div_fp = div_iwr.find(class_="col-lg-5 text-sm-right more_info_container mt-4 mt-lg-0")   #一串
+#print(div_fp)  
+t_sma = div_fp.find_all("small" ,limit=1)      #陣列
+wti_oil_date=""
+for wti_d in t_sma:
+    wti_oil_date = wti_d.text.lstrip()   #去空白
+    wti_oil_date = wti_oil_date[11:21]   #擷取字串位置
+#print(wti_oil_date)
+re_wti_oil_date = wti_oil_date.replace('/', '-')   #替代日期斜線換-
+final_result = '國際原油價格-西德州(WTI): '+re_wti_oil_date+' USD/桶$:'+wti_oil_price
+print(final_result)
+
+ 
